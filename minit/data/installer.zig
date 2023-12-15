f4009a7b976c90a50215bd07749bb759862f9f87
Josu Igoa <josuigoa@gmail.com>
2023-12-15 09:54:14 +0100

    [minit] instalatzailea gehitua

---
+++ minit/data/installer.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const utils = @import("utils");
+const eu_txt = @embedFile("./minit_loc.csv");
+
+pub fn get_game_names() [5]utils.string {
+    return [_]utils.string{ "minit", "MINIT", "Minit", "", "" };
+}
+
+pub fn install_translation(game_path: utils.string) !void {
+    const loc_path = utils.look_for_file(game_path, "minit_loc.csv") catch "";
+
+    std.debug.print("[{s}] fitxategia kopiatzen...\n", .{loc_path});
+    const file_txt = try std.fs.cwd().createFile(loc_path, .{});
+
+    defer file_txt.close();
+
+    _ = try file_txt.write(eu_txt);
+}
